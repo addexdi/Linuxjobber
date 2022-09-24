d1918385da4e6fda850305fc5a39364142d0c45e
[Adam Ibrahim] <onelordad@gmail.com>
2022-09-24 02:10:31 +0100

    react app

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Adam",
+      skill: "React"
+    }
+
+    this.handleChange = (e) => {
+      this.setState({
+        name: e.target.value
+      })
+    }
+
+    this.handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('this form was submited by', this.state.name)
+    }
+
+  }
+
+
+  render() {
+    return(
+      <div className='App'>
+        <h1>My name is {this.state.name}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="enter name" onChange={this.handleChange}></input>
+            <button>Submit</button> 
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
